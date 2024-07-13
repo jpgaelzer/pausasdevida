ebb43d44388ea7cfb43bfc4fc0f189c8ed359d1a
jpcoelhosenac <jpcoelho@senacrs.com.br>
2024-07-13 01:52:39 -0300

    90% pronto

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,35 @@ $(function (){
         if(debug === true) return console.log(string);
     }
 
+    $(".abrirMenu").click(function (){
+        $(".menuFlutuante").addClass("ativo");
+        $("#app").append($("<div class='wall'></div>"));
+    });
+    $("body").on("touchstart click", ".fecharMenu, .wall", function (){
+        
+        fecharMenu();
+     });
+
+     $(".sobreProjeto").click(function(){
+        rolarPara("#perfil");
+        fecharMenu();
+
+     });
+    var fecharMenu = ()=> {
+        fecharWall();
+        $(".menuFlutuante").removeClass("ativo");
+
+    } 
+    var fecharWall = ()=>{
+    $(".wall").addClass("sair").delay(900).promise().done(function (){
+        $(this).remove();
+    })
+    }
+     var rolarPara = function (destino){
+        $('html, body').animate({
+            scrollTop: $(destino).offset().top
+        }, 1000); // 1000 milissegundos = 1 segundo de duração da animação
+     }
     // log($(window).height());
     // var ajustarAlturaPausa = function (){
 
